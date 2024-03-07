525005193b6a8e8887ebfa5b901f0cfb5c00972f
synthpop123 <2020393267@qq.com>
2024-03-07 22:14:13 +0800

    add webp cloud

---
+++ convert.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from bs4 import BeautifulSoup
+
+def replace_img_urls(directory, old_host, new_host):
+  for root, dirs, files in os.walk(directory):
+    for file in files:
+      if file.endswith('.html'):
+        file_path = os.path.join(root, file)
+        with open(file_path, 'r') as f:
+          soup = BeautifulSoup(f, 'html.parser')
+
+          img_tags = soup.find_all('img')
+
+          for img_tag in img_tags:
+            if img_tag.has_attr('src'):
+              src = img_tag['src']
+              if src.startswith(old_host):
+                img_tag['src'] = new_host + src[len(old_host):]
+              elif src.startswith('/'):
+                img_tag['src'] = new_host + src[1:]
+
+            if img_tag.has_attr('data-srcset'):
+              src = img_tag['data-srcset']
+              if src.startswith(old_host):
+                img_tag['data-srcset'] = new_host + src[len(old_host):]
+              elif src.startswith('/'):
+                img_tag['data-srcset'] = new_host + src[1:]
+
+          with open(file_path, 'w') as output_file:
+            output_file.write(str(soup))
+
+if __name__ == '__main__':
+  if len(sys.argv) < 4:
+    print("Usage: python3 convert.py <directory> <old_host> <new_host>")
+    sys.exit(1)
+
+  directory = sys.argv[1]
+  old_host = sys.argv[2]
+  new_host = sys.argv[3]
+
+  replace_img_urls(directory, old_host, new_host)
