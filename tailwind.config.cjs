3348a27c08a084c8d2d2692a06a910aba3811b27
synthpop123 <2020393267@qq.com>
2024-04-15 23:25:00 +0800

    change site font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["LXGW WenKai Screen", "monospace"],
       },
 
       typography: {
