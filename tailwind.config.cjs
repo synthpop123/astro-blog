5d5f87bed6f4794d0ceec8fc4047ff2f1999c65f
synthpop123 <2020393267@qq.com>
2024-06-19 23:54:27 +0800

    feat: Add astro-font to handle mono-font in order to replace google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["var(--font-ibm)"],
         fix: ["LXGW WenKai Screen"],
       },
 
