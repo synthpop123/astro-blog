a55b21c5423c5c7832dbfcbeb60df3d89aa8aaca
synthpop123 <2020393267@qq.com>
2024-07-01 22:28:20 +0800

    feat: Update language settings to use Chinese (zh) as default

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,8 @@ export const SITE: Site = {
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "zh", // html lang code. Set this empty and default will be "en"
+  langTag: ["zh-CN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
