c13cc854c14fa2a279911a08d46740cb40a4760c
synthpop123 <2020393267@qq.com>
2024-07-30 01:16:26 +0800

    feat: Add KaTeX support in AstroPaper blog post layout

--- src/content/config.ts
+++ src/content/config.ts
@@ -20,6 +20,7 @@ const blog = defineCollection({
         .optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
+      katex: z.boolean().default(false),
     }),
 });
 
